14ba4e7bb16b890f1d00bbe274c6622222fb4d7e
antonio-fiorentino <antoniofiorentino16@gmail.com>
2021-01-13 17:51:35 +0100

    Esercizio

--- js/main.js
+++ js/main.js
@@ -10,9 +10,4 @@ $(document).ready(function() {
     $('img.active').removeClass('active').prev().addClass('active');
     $('i.active').removeClass('active').prev().addClass('active')
   })
-  // var next = getElementsByClassName('next');
-  // var prev = getElementsByClassName('prev')
-  // if (click(next)) {
-  //
-  // }
 })
